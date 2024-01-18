16bfb5959a69c9f3447cda335a997bb68a18be1c
Gorbunov-Danila <gorbunovdanil345@gmail.com>
2024-01-18 14:45:56 +0300

    1

---
+++ prog/PointsCover1.py
@@ -0,0 +1,12 @@
+
+if __name__ == '__main__':
+    S = [float(x) for x in input("Введите список чисел через пробел: ").split()]
+    result = []
+
+    while S:
+        x = float(min(S))
+        result.append([x, x + 1])
+
+        S = [num for num in S if num < x or num > x + 1]
+
+    print(result)
